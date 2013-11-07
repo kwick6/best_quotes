773fe6b9bdc0bd14095006138b4bf9f8c64a2dba
SG <sy1840@gmail.com>
2013-11-06 20:13:52 -0800

    chp 6 and 7

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,8 +2,42 @@ class QuotesController < Rulers::Controller
   def a_quote
     render :a_quote, :noun => :winking
   end
-  
+
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
+  end
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def update_quote
+    attrs = { "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.save attrs
+    render :quote, :obj => m
+  end
+
+  def new_quote
+    attrs = { "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
   def exception
-        raise "It's a bad one!"
+    raise "It's a bad one!"
   end
 end
